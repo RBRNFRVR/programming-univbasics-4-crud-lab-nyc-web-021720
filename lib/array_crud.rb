8be288b4a948fd1b192f9b0392ede13755608b7b
Joseph Chavez <rommelchavez99@gmail.com>
2020-02-12 00:51:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -44,5 +44,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-new_array[2] = "Tasmanian Devil"
+new_array[-1] = "Tasmanian Devil"
 end
